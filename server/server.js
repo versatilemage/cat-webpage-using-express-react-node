46a5597d2f20434f6b707d3097a67673fd95e531
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-18 23:37:05 +0530

    server for API collection using backend

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const axios = require("axios")
+const PORT = 3001
+const cors = require("cors")
+const app = express()
+const API_KEY = "81087f92-e521-48ba-b151-fc3c0f9095b1"
+const Url = `https://api.thecatapi.com/v1/images/search?limit=100&api_key=${API_KEY}`
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+app.get("/kitties",(req,res) => {
+    axios(Url)
+        .then(response => {
+            const catdetails = response.data
+            res.json(catdetails)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT,() => console.log(`express server is running on ${PORT}`))
