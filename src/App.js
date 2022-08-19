c136f1b818ce886668945f162e95218009245cbb
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-19 23:27:15 +0530

    added new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./components/home";
+import toplist from "./components/topten";
+// import { addcat, getcatDetails } from "./features/createslice";
+// import { useSelector, useDispatch } from "react-redux/es/exports";
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="toptencats" element={<toplist />}/>
       </Routes>
     </>
   );
