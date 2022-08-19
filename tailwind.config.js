32e46c467af2de6b53bf9d845c5b5b0453d2cca8
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-19 23:28:18 +0530

    added image for accessing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        "cat-img": "url('/img/HeroImagelg.png')",
+        "img-1": "url('/img/image 1.png')",
+        "img-2": "url('/img/image 2.png')",
+        "img-3": "url('/img/image 3.png')",
+      })
+    },
   },
   plugins: [],
 }
