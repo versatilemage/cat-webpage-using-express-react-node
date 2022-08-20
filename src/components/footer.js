cdb79442d950add4c07670bd820002ea32c0a725
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-20 23:35:29 +0530

    footer component for a common footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Bottomheader() {
+    return (
+        <>
+            <footer className="bg-black flex flex-row justify-between py-10 rounded-t-2xl">
+                <h2 className="text-white text-2xl font-bold ml-5">Cat Lover's</h2>
+                <h2 className="text-white text-xl mr-5">not licensed</h2>
+            </footer>
+        </>
+    )
+}
+
+export default Bottomheader
