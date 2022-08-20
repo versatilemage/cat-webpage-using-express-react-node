b0ce238bbe1f67d488596d477087c9d33bea53f6
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-20 23:34:40 +0530

    changed the component name and imported new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/home";
-import toplist from "./components/topten";
+import Selectedlist from "./components/selectedbreed";
+import topten from "./components/topten";
 // import { addcat, getcatDetails } from "./features/createslice";
 // import { useSelector, useDispatch } from "react-redux/es/exports";
 
@@ -10,7 +11,8 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />}/>
-        <Route path="toptencats" element={<toplist />}/>
+        <Route path="selectedBreed" element={<Selectedlist />}/>
+        <Route path="" element={<topten />}/>
       </Routes>
     </>
   );
