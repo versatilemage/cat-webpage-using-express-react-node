8f4eb04ad5feb9fb7711f6f37cd0a0c2e625f363
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-21 23:52:20 +0530

    Changed the component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/home";
 import Selectedlist from "./components/selectedbreed";
-import topten from "./components/topten";
+import Topten from "./components/topten";
 // import { addcat, getcatDetails } from "./features/createslice";
 // import { useSelector, useDispatch } from "react-redux/es/exports";
 
@@ -11,8 +11,8 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />}/>
-        <Route path="selectedBreed" element={<Selectedlist />}/>
-        <Route path="" element={<topten />}/>
+        <Route path="/selectedBreed" element={<Selectedlist />}/>
+        <Route path="/topten" element={<Topten />}/>
       </Routes>
     </>
   );
