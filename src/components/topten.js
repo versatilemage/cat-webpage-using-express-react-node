bc3495fe00e385d7ede1e37e4cbd89998cdc193b
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-08-21 23:53:54 +0530

    made a model for top 10 cat breeds

--- src/components/topten.js
+++ src/components/topten.js
@@ -1,9 +1,30 @@
-function toplist() {
+function Topten () {
     return (
         <>
-            <h2>top 10</h2>
+            <h2 className="text-2xl font-bold ml-5 capitalize">top ten most searched breeds</h2>
+            <div className="flex flex-col space-y-5">
+                <div className="grid grid-cols-2 space-x-5 mx-5 my-10">
+                    <div className="bg-red-300 items-start flex flex-col space-y-5">
+                        <div className="px-10 py-32 bg-slate-500 rounded-2xl w-2/4"></div>
+                    </div>
+                    <div className="bg-blue-300">
+                        <h3 className="capitalize text-xl font-bold">breed of the cat</h3>
+                        <p className="w-3/4 items-justify my-5 text-lg">iucliuclhifdiubiubviudbviwbcuiwab dcihcsichaohclsdc sdliucsuciwahc sdiuchsvhdhv sdichshchc sduhcsdhds</p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 space-x-5 mx-5 my-10">
+                    <div className="bg-red-300 items-start flex flex-col space-y-5">
+                        <div className="px-10 py-32 bg-slate-500 rounded-2xl w-2/4"></div>
+                    </div>
+                    <div className="bg-blue-300">
+                        <h3 className="capitalize text-xl font-bold">breed of the cat</h3>
+                        <p className="w-3/4 items-justify my-5 text-lg">iucliuclhifdiubiubviudbviwbcuiwab dcihcsichaohclsdc sdliucsuciwahc sdiuchsvhdhv sdichshchc sduhcsdhds</p>
+                    </div>
+                </div>
+            </div>
+            
         </>
     )
 }
 
-export default toplist
+export default Topten
